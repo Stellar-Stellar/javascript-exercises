cae500146491fc539157688f3ea85a3cdb97d965
Stellar-Stellar <86974310+Stellar-Stellar@users.noreply.github.com>
2023-03-24 18:56:43 -0500

    Finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = ""
+    let position = string.length
+    for (let i = 0; i <= string.length; i++) {
+        reversed += string.charAt(position)
+        position = position - 1
+    }
+    return reversed
 };
 
 // Do not edit below this line
