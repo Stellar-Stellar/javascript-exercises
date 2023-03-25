04d3d4227350d061664d2807bafbc4a2853c6e6a
Stellar-Stellar <86974310+Stellar-Stellar@users.noreply.github.com>
2023-03-25 18:09:41 -0500

    Finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    let max
+    let min
+    let total = 0
+    if(arg1 > arg2){
+        max = arg1
+        min = arg2
+    } else if(arg1 < arg2) {
+        max = arg2
+        min = arg1
+    } 
+    for(let i = min; i <= max; i++){
+        total += i
+    }
+    if (arg1 < 0 || arg2 < 0){
+        total = "ERROR"
+    }
+    if (Number.isInteger(arg1) && Number.isInteger(arg2)){
+        return total
+    } else {
+        total = "ERROR"
+    }
+    return total
 
 };
 
